8b6499b0102e145a1530abdc85d620e58ca7e131
GrahamBrown-OregonState <browgrah@oregonstate.edu>
2026-03-09 14:44:34 -0700

    add technoligies animation and section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import HeroText from "@/components/HeroText";
+import Projects from "@/components/Projects";
+import Technologies from "@/components/Technologies";
 
 export default function Home() {
   return (
     <main>
       <HeroText />
+      <Technologies />
+      <Projects />
+
     </main>
   );
 }
